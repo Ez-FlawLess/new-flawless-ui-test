e48c0f163fc7ccb13c2412f9c1268665bb346562
aa.mohammadi <amirali23mohammadi2@gmail.com>
2023-07-04 14:25:46 +0330

    /.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
 
-import { FlawLessUI, Loading, useLoading, createConfig, HTTP_METHODS, HttpFeedback, AlertI, useHttp } from 'flawless-ui'
+import { FlawLessUI, useLoading, createConfig, HTTP_METHODS, GlobalHttpFeedback, AlertI, useHttp } from 'flawless-ui'
 import axios from 'axios';
 import { FC, ReactElement, ReactNode } from 'react';
 import { useEffect } from 'react';
 
 const api = axios.create({
-  baseURL: 'https://dummyjson.com',
-})
-
-const secondaryApi = axios.create({
   baseURL: 'https://api.selfit.ir/api/Global/v1/',
 })
 
@@ -21,7 +17,8 @@ const config = createConfig({
       success: (props: AlertI) => <div>S - {props.title} - {props.message}<button onClick={props.onClose}>close</button> {props.props?.hi}</div>,
       error: (props: AlertI) => <div>E - {props.title} - {props.message}<button onClick={props.onClose}>close</button></div>,
     }
-  }
+  },
+  globalHttpFeedback: true,
 })
 
 function App() {
@@ -30,7 +27,7 @@ function App() {
     <div className="App">
       <FlawLessUI config={config}>
         <Test />
-      </FlawLessUI>2
+      </FlawLessUI>
     </div>
   );
 }
@@ -43,6 +40,7 @@ export const Test: FC = props => {
     Feedback,
     id,
   } = useHttp()
+  const t = useHttp()
 
   useEffect(() => {
     getData()
@@ -63,6 +61,14 @@ export const Test: FC = props => {
       <AnotherTest 
         id={id}
       />
+      <GlobalHttpFeedback>
+        {(fs: any) => fs.map((f: any) => (
+          <h1 onClick={f.onClose}>
+            hi
+            {f.message}
+          </h1>
+        ))}
+      </GlobalHttpFeedback>
     </>
   )
 }
@@ -72,6 +78,7 @@ export const AnotherTest: FC<{
 }> = props => {
 
   const loading = useLoading(props.id)
+  const t = useHttp()
 
   return (
     <h1 >
